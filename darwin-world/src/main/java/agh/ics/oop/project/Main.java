261157ccdc73f8638e5eb0fe48f514ef5e96853c
Szymon Szarek <44586881+szaross@users.noreply.github.com>
2023-12-18 16:43:44 +0100

    console map print test

--- darwin-world/src/main/java/agh/ics/oop/project/Main.java
+++ darwin-world/src/main/java/agh/ics/oop/project/Main.java
@@ -1,8 +1,6 @@
 package agh.ics.oop.project;
 
-import agh.ics.oop.project.model.Direction;
-import agh.ics.oop.project.model.Genotype;
-import agh.ics.oop.project.model.WorldMap;
+import agh.ics.oop.project.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +8,17 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         WorldMap map = new WorldMap(10,10,1);
+        map.placePlant(new Plant(2,2,2));
+        List<Animal> animals = new ArrayList<>(List.of(new Animal(new Vector2d(2,7),5,1),new Animal(new Vector2d(3,5),5,1)));
+        for (Animal animal : animals){
+            System.out.println(animal.getGenotype().getGenes());
+            map.placeAnimal(animal);
+        }
         System.out.println(map);
+        for (int i = 0; i < 5; i++) {
+            map.updateMap();
+            System.out.println(map);
+        }
+
     }
 }
