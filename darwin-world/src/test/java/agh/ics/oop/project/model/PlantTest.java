50ec9fee7219e66386ef77f8956c622177a58a3c
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-12-18 10:23:22 +0100

    Added tests

---
+++ darwin-world/src/test/java/agh/ics/oop/project/model/PlantTest.java
@@ -0,0 +1,37 @@
+package agh.ics.oop.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlantTest {
+    @Test
+    public void getPositionTest()
+    {
+        Plant plant1 = new Plant(4,5,40);
+        Plant plant2 = new Plant(7,-3,40);
+        assertEquals(new Vector2d(4,5), plant1.getPosition());
+        assertEquals(new Vector2d(7,-3), plant2.getPosition());
+    }
+
+    @Test
+    public void getEnergyTest()
+    {
+        Plant plant1 = new Plant(4,5,40);
+        Plant plant2 = new Plant(7,-3,60);
+        assertEquals(40, plant1.getEnergy());
+        assertEquals(60, plant2.getEnergy());
+    }
+
+    @Test
+    public void setEnergyTest()
+    {
+        Plant plant1 = new Plant(4,5,40);
+        Plant plant2 = new Plant(7,-3,40);
+        plant1.setEnergy(70);
+        plant2.setEnergy(20);
+        assertEquals(70, plant1.getEnergy());
+        assertEquals(20, plant2.getEnergy());
+    }
+
+}
---
+++ darwin-world/src/test/java/agh/ics/oop/project/model/TileTest.java
@@ -0,0 +1,31 @@
+package agh.ics.oop.project.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TileTest {
+    private Animal animal;
+    private Tile tile;
+
+    @BeforeEach
+    public void setUp(){
+        animal = new Animal(new Vector2d(2,5), 40, 20);
+        tile = new Tile();
+    }
+    @Test
+    public void addTest(){
+        tile.addAnimal(animal);
+        assertTrue(tile.animals.contains(animal));
+    }
+
+    @Test
+    public void removeTest(){
+        tile.addAnimal(animal);
+        tile.removeAnimal(animal);
+        assertFalse(tile.animals.contains(animal));
+    }
+
+}
