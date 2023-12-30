ac377f8a17bc76fa540a4373220b5649a107d04a
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-12-30 20:52:56 +0100

    config update

--- darwin-world/src/main/java/agh/ics/oop/project/presenter/SettingsPresenter.java
+++ darwin-world/src/main/java/agh/ics/oop/project/presenter/SettingsPresenter.java
@@ -48,6 +48,75 @@ public class SettingsPresenter {
     @FXML
     private Text valueText;
 
+    @FXML
+    public void initialize() {
+        updateValueText();
+
+        sizeX.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        sizeY.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        initialPlantCount.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        initialPlantEnergy.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        growPerDay.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        animalCount.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        animalEnergy.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        reproduceEnergy.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        reproduceLoss.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        energyLoss.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        genomeLen.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+
+        turnTime.valueProperty().addListener((observable, oldValue, newValue) -> {
+            updateValueText();
+        });
+    }
+    private void updateValueText() {
+        String textBuilder = "Szerokosc mapy: " + (int) sizeX.getValue() + "\n" +
+                "Wysokosc mapy: " + (int) sizeY.getValue() + "\n" +
+                "Startowa ilosc roslin: " + (int) initialPlantCount.getValue() + "\n" +
+                "Energia roslin: " + (int) initialPlantEnergy.getValue() + "\n" +
+                "Codzienny wzrost roslin: " + (int) growPerDay.getValue() + "\n" +
+                "Ilosc zwierzat: " + (int) animalCount.getValue() + "\n" +
+                "Poczatkowa energia zwierzat: " + (int) animalEnergy.getValue() + "\n" +
+                "Energia potrzebna do rozmnazania: " + (int) reproduceEnergy.getValue() + "\n" +
+                "Strata energii podczas rozmnazania: " + (int) reproduceLoss.getValue() + "\n" +
+                "Codzienna strata energii: " + (int) energyLoss.getValue() + "\n" +
+                "Dlugosc genomu: " + (int) genomeLen.getValue() + "\n" +
+                "Czas dnia w milisekundach: " + (int) turnTime.getValue() + "\n";
+
+        valueText.setText(textBuilder);
+    }
+
     public void onSimulationStartClicked() {
         //Test print
         valueText.setText((int) sizeX.getValue() + " " + (int) sizeY.getValue() + " " + (int) initialPlantCount.getValue() + " " + (int) initialPlantEnergy.getValue());
